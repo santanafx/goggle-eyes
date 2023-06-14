d004eeacddb3d0648b51bffd4f2ba15b06949f08
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-06-14 12:38:10 -0300

    Atualização no código de Slider
    Foi criada a animação de dar zoom no slide e gerar efeito de blink ao passar de imagem. O efeito blink funciona dinâmicamente apertando as arrows, depois que a função timeout roda por 5 segundos e ao clicar em algum ponto.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,22 +7,35 @@ import { VscCircleLargeFilled } from "react-icons/vsc";
 export const Slider = ({ slides }) => {
 
     const [currentIndex, setCurrentIndex] = React.useState(0);
-    const timerRef = React.useRef();
+    const [animation, setAnimation] = React.useState(false);
+    const timerRef = React.useRef(null);
+
+    React.useEffect(() => {
+        clearTimeout(timerRef.current)
+        timerRef.current = setTimeout(() => {
+            goToNext();
+            setAnimation(!animation);
+        }, 5000)
+    },)
 
     const goToNext = () => {
         if (currentIndex === slides.length - 1) {
-            setCurrentIndex(0)
+            setCurrentIndex(0);
+            setAnimation(!animation);
         } else {
-            setCurrentIndex(currentIndex + 1)
+            setCurrentIndex(currentIndex + 1);
+            setAnimation(!animation);
         }
     }
 
     const goToPrevious = () => {
         if (currentIndex > 0) {
             setCurrentIndex(currentIndex - 1)
+            setAnimation(!animation);
         }
         if (currentIndex === 0) {
             setCurrentIndex(slides.length - 1)
+            setAnimation(!animation);
         }
     }
 
@@ -32,14 +45,19 @@ export const Slider = ({ slides }) => {
 
     return (
         <div className={styles.sliderContainer}>
-            <img src={slides[currentIndex].url} alt="" />
-            <div onClick={() => goToNext()}>
+            {animation ? <div className={styles.sliderAnimationImg}
+                style={{ backgroundImage: `url(${slides[currentIndex].url})` }}>
+            </div> : ''}
+            {animation ? '' : <div className={styles.sliderAnimationImg}
+                style={{ backgroundImage: `url(${slides[currentIndex].url})` }}>
+            </div>}
+            <div className={styles.sliderRightArrow} onClick={() => goToNext()} tabIndex={1}>
                 <AiOutlineCaretRight size='60' color='white' />
             </div>
-            <div onClick={() => goToPrevious()}>
+            <div className={styles.sliderLeftArrow} onClick={() => goToPrevious()} tabIndex={2}>
                 <AiOutlineCaretLeft size='60' color='white' />
             </div>
-            <div>
+            <div className={styles.sliderDots}>
                 {slides.map((element, index) => (
                     <VscCircleLargeFilled onClick={() => goToIndex(index)} style={{ margin: '0px 10px' }} size='20' color='white' key={element + index} />
                 ))}
